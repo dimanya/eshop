2275540c9a9304938d6f53da442c5d3e6725a015
dimanya <dimanya35@gmail.com>
2023-05-19 10:40:33 +0500

    done

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require("mongoose")
+
+const schema = new Schema({
+    name: {type: String},
+    email: {type: String, required: true, unique: true},
+    password: {type: String},
+    image: String
+}, {
+    timestamps: true
+})
+
+module.exports = model("User", schema)
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const router = express.Router({mergeParams: true})
 
+router.use("/auth",  require("./auth.routes"))
 router.use("/flowers", require("./flowers.routes"))
 
 module.exports = router
